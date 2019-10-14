f429bb7bfa893a67f039d2eb9f269b46644a160e
jair <rivero_jair@hotmail.com>
2019-10-15 02:30:13 +1200

    Codigo- Resolucion del T2

--- src/main/java/pe/edu/upn/marriott/controller/ClienteController.java
+++ src/main/java/pe/edu/upn/marriott/controller/ClienteController.java
@@ -1,12 +1,14 @@
 package pe.edu.upn.marriott.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
@@ -32,10 +34,19 @@ public class ClienteController {
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
-		return "/Cliente/ListadoCliente";
+		return "/cliente/ListadoCliente";
 	}
 	
-	@PostMapping("/registro")
+	@GetMapping("/nuevo")
+	public String nuevo(Model model) {
+		Cliente cliente = new Cliente();
+		
+		model.addAttribute("cliente", cliente);
+		
+		return "/cliente/registro";
+	}
+	
+	@PostMapping("/save")
 	public String save(@ModelAttribute("cliente") Cliente cliente, Model model, SessionStatus status) {
 		try {
 			clienteService.save(cliente);
@@ -44,7 +55,45 @@ public class ClienteController {
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
-		return "redirect:/Clientes";
+		return "redirect:/cliente";
+	}
+	
+	@GetMapping("/del/{id}")
+	public String eliminar(@PathVariable("id") int id, Model model) {
+		try {
+			Optional<Cliente> cliente = clienteService.findById(id);
+			if(cliente.isPresent()) {
+				clienteService.deleteById(id);
+			}
+		} catch (Exception e) {
+			model.addAttribute("dangerDel", "ERROR - Violacion contra el principio de Integridad Referencial");
+			try {
+				List<Cliente> clientes = clienteService.findAll();
+				model.addAttribute("medicos", clientes);
+			} catch (Exception e2) {
+				// TODO: handle exception
+			}
+			return "/cliente/ListadoCliente";
+		}
+		
+		return "redirect:/cliente";
+	}
+	
+	@GetMapping("/edit/{id}")
+	public String editar(@PathVariable("id") int id, Model model) {
+		try {
+			Optional<Cliente> optional = clienteService.findById(id);
+			if (optional.isPresent()) {
+				
+				model.addAttribute("cliente", optional.get());
+			} else {
+				return "redirect:/cliente";
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		return "/cliente/edit";
 	}
 	
 	
