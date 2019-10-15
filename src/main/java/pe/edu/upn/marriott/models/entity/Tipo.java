96187dc826e25cc8493488064d7a9598be085de8
Andres Castelo <telescopio322@gmail.com>
2019-10-14 19:17:58 -0500

    n00062039

--- src/main/java/pe/edu/upn/marriott/models/entity/Tipo.java
+++ src/main/java/pe/edu/upn/marriott/models/entity/Tipo.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
@@ -19,41 +20,10 @@ public class Tipo {
 	
 	@Column(name = "nombre", length = 30)
 	private String nombre;
-	
-	@OneToMany(mappedBy = "tipo")
+
+	@OneToMany(mappedBy = "tipo", fetch = FetchType.LAZY)
 	private List<Habitacion> habitaciones;
 	
-	public Tipo() {
-		this.habitaciones = new ArrayList<>();
-	}
 	
-	public void addHabitacion( Habitacion habitacion ) {
-		habitacion.setTipo( this );
-		this.habitaciones.add( habitacion );
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public List<Habitacion> getHabitaciones() {
-		return habitaciones;
-	}
-
-	public void setHabitaciones(List<Habitacion> habitaciones) {
-		this.habitaciones = habitaciones;
-	}
 
 }
--- src/main/java/pe/edu/upn/marriott/models/entity/Vendedor.java
+++ src/main/java/pe/edu/upn/marriott/models/entity/Vendedor.java
@@ -1,8 +1,12 @@
 package pe.edu.upn.marriott.models.entity;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -27,6 +31,10 @@ public class Vendedor {
 	
 	@Column(name = "sueldo")
 	private Float sueldo;
+	
+	@OneToMany(mappedBy = "habitacion", fetch = FetchType.LAZY)
+	private List<Alquiler> alquileres;
+	
 	
 	
 	
