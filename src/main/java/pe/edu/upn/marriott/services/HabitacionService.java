9a1eccaac58c1c18423ca8a2f126ffdf50930e1f
fabrizzio aranda <fabrizzio1996@hotmail.es>
2019-10-14 19:18:08 -0500

    N00047143

--- src/main/java/pe/edu/upn/marriott/services/HabitacionService.java
+++ src/main/java/pe/edu/upn/marriott/services/HabitacionService.java
@@ -1,7 +1,10 @@
 package pe.edu.upn.marriott.services;
 
-import pe.edu.upn.marriott.models.entity.Habitacion;
+import org.springframework.stereotype.Service;
 
+import pe.edu.upn.marriott.models.entity.Habitacion;
+@Service
 public interface HabitacionService extends CrudService<Habitacion, Integer>{
 
+	
 }
