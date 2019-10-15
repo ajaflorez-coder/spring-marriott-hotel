46b118e60364de2f00629c5425477d82a47ea8d3
Jair Rivero Palomino <rivero_jair@hotmail.com>
2019-10-14 19:14:27 -0500

    T2 avance

---
+++ src/main/java/pe/edu/upn/marriott/controller/ClienteController.java
@@ -0,0 +1,51 @@
+package pe.edu.upn.marriott.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+import pe.edu.upn.marriott.models.entity.Cliente;
+import pe.edu.upn.marriott.services.ClienteService;
+
+@Controller
+@RequestMapping("/cliente")
+@SessionAttributes({"cliente"})
+
+public class ClienteController {
+	
+	@Autowired
+	private ClienteService clienteService;
+	
+	@GetMapping
+	public String inicio(Model model) {
+		try {
+			List<Cliente> clientes = clienteService.findAll();
+			model.addAttribute("clientes", clientes);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return "/Cliente/ListadoCliente";
+	}
+	
+	@PostMapping("/registro")
+	public String save(@ModelAttribute("cliente") Cliente cliente, Model model, SessionStatus status) {
+		try {
+			clienteService.save(cliente);
+			status.setComplete();
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return "redirect:/ListadoCliente";
+	}
+	
+	
+}
