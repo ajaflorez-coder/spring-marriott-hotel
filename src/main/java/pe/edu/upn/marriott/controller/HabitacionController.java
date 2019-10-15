f9db53d886f57a1ffdc936522c17714ff20aa76c
Brijitte Garcia <brijitte.joanne.2597@gmail.com>
2019-10-14 19:15:02 -0500

    t2

---
+++ src/main/java/pe/edu/upn/marriott/controller/HabitacionController.java
@@ -0,0 +1,160 @@
+package pe.edu.upn.marriott.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+
+import pe.edu.upn.marriott.models.entity.Alquiler;
+import pe.edu.upn.marriott.models.entity.Habitacion;
+import pe.edu.upn.marriott.services.AlquilerService;
+import pe.edu.upn.marriott.services.HabitacionService;
+
+
+
+@Controller
+@RequestMapping("/habitacion")
+@SessionAttributes( {"habitacion" , "alquileres"} )
+public class HabitacionController {
+
+	
+	@Autowired
+	private AlquilerService alquilerService;
+	
+	@Autowired
+	private HabitacionService habitacionService;
+
+	@GetMapping
+	public String inicio(Model model) {
+		try {
+			List<Habitacion> habitaciones = habitacionService.findAll();
+			model.addAttribute("habitaciones", habitaciones);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return "/habitacion/inicio";
+	}
+	
+	@GetMapping("/edit/{id}")
+	public String editar(@PathVariable("id") int id, Model model) {
+		try {
+			Optional<Habitacion> optional = habitacionService.findById(id);
+			if (optional.isPresent()) {
+				
+				List<Alquiler> alquileres
+					= alquilerService.findAll(); 
+				
+				model.addAttribute("alquiler", optional.get());
+				model.addAttribute("alquileres", alquileres);
+			} else {
+				return "redirect:/medico";
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		return "/habitacion/edit";
+	}
+	
+	@PostMapping("/save")
+	public String save(@ModelAttribute("habitacion") Habitacion habitacion, 
+			Model model, SessionStatus status) {
+		try {
+			habitacionService.save(habitacion);
+			status.setComplete();
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return "redirect:/habitacion";
+	}
+	@GetMapping("/nuevo")
+	public String nuevo(Model model) {
+		Habitacion habitacion = new Habitacion();
+		model.addAttribute("habitacion", habitacion);
+		try {
+			List<Alquiler> alquileres
+			= alquilerService.findAll(); 
+			model.addAttribute("alquileres", alquileres);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return "/alquiler/nuevo";
+	}
+	
+	@GetMapping("/del/{id}")
+	public String eliminar(@PathVariable("id") int id, Model model) {
+		try {
+			Optional<Habitacion> habitacion= habitacionService.findById(id);
+			if(habitacion.isPresent()) {
+				habitacionService.deleteById(id);
+			}
+		} catch (Exception e) {
+			
+			model.addAttribute("dangerDel", "ERROR - Violación contra el principio de Integridad referencia");
+			try {
+				List<Habitacion> habitaciones = habitacionService.findAll();
+				model.addAttribute("habitaciones", habitaciones);
+			} catch (Exception e2) {
+				// TODO: handle exception
+			} 
+			return "/habitacion/inicio";
+		}
+		return "redirect:/habitacion";
+	}
+	@GetMapping("/info/{id}")
+	public String info(@PathVariable("id") int id, Model model) {
+		try {
+			Optional<Habitacion> habitacion = habitacionService.findById(id);
+			if(habitacion.isPresent()) {
+				model.addAttribute("habitacion", habitacion.get());
+			} else {
+				return "redirect:/habitacion";
+			}
+		} catch (Exception e) {
+
+		}	
+		
+		return "/habitacion/info";
+	}
+	@GetMapping("/{id}/nuevoalquiler")
+	public String nuevoAlquiler(@PathVariable("id") int id, Model model) {
+		Alquiler alquiler= new Alquiler();
+		try {
+			Optional<Habitacion> habitacion = habitacionService.findById(id);
+			if(habitacion.isPresent()) {
+				alquiler.setHabitaciones(habitacion.get());
+				model.addAttribute("alquiler", alquiler);
+			} else {
+				return "redirect:/medico";
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return "/medico/nuevopaciente";
+	}
+	
+	@PostMapping("/savealquiler")
+	public String savePaciente(@ModelAttribute("paciente") Alquiler alquiler, 
+			Model model, SessionStatus status) {
+		try {
+			alquilerService.save(alquiler);
+			status.setComplete();
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return "redirect:/medico/info/" + alquiler.getHabitaciones().getId();
+	}
+	
+}
