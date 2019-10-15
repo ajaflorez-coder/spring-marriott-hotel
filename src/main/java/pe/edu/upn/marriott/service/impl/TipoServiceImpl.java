ec6e270e2d9c0eb81953ab1dadf6f0073044a1d9
ronin45capc19 <beto12-u1@hotmail.com>
2019-10-15 02:58:49 -0400

    codigo - resolucion t2

---
+++ src/main/java/pe/edu/upn/marriott/service/impl/TipoServiceImpl.java
@@ -0,0 +1,62 @@
+package pe.edu.upn.marriott.service.impl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import pe.edu.upn.marriott.models.entity.Tipo;
+import pe.edu.upn.marriott.models.repository.TipoRepository;
+import pe.edu.upn.marriott.services.TipoService;
+
+@Service
+public class TipoServiceImpl implements TipoService{
+	
+	@Autowired
+	private TipoRepository tipoRepository;
+	
+	@Transactional(readOnly = true)
+	@Override
+	public List<Tipo> findAll() throws Exception {
+		// TODO Auto-generated method stub
+		return tipoRepository.findAll();
+	}
+	
+	@Transactional(readOnly = true)
+	@Override
+	public Optional<Tipo> findById(String id) throws Exception {
+		// TODO Auto-generated method stub
+		return tipoRepository.findById(id);
+	}
+	
+	@Transactional
+	@Override
+	public Tipo save(Tipo entity) throws Exception {
+		// TODO Auto-generated method stub
+		return tipoRepository.save(entity);
+	}
+
+	@Transactional
+	@Override
+	public Tipo update(Tipo entity) throws Exception {
+		// TODO Auto-generated method stub
+		return tipoRepository.save(entity);
+	}
+
+	@Transactional
+	@Override
+	public void deleteById(String id) throws Exception {
+		// TODO Auto-generated method stub
+		tipoRepository.deleteById(id);
+	}
+
+	@Transactional
+	@Override
+	public void deleteAll() throws Exception {
+		// TODO Auto-generated method stub
+		tipoRepository.deleteAll();
+	}
+
+}
--- src/test/java/pe/edu/upn/marriott/MarriottApplicationTests.java
+++ src/test/java/pe/edu/upn/marriott/MarriottApplicationTests.java
@@ -7,24 +7,40 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import pe.edu.upn.marriott.models.entity.Habitacion;
+import pe.edu.upn.marriott.models.entity.Tipo;
 import pe.edu.upn.marriott.models.repository.HabitacionRepository;
+import pe.edu.upn.marriott.models.repository.TipoRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MarriottApplicationTests {
 	@Autowired
 	private HabitacionRepository habitacionRepository;
+	@Autowired
+	private TipoRepository tipoRepository;
 	@Test
 	public void contextLoads() {
 		try {
+			Tipo tipo1=new Tipo();
+		    tipo1.setId("j001");
+			tipo1.setNombre("jota");
+			
+			tipoRepository.save(tipo1);
+			
 			Habitacion habitacion1=new Habitacion();
 			habitacion1.setNumeroCamas(23);
 			habitacion1.setDescripcion("locazo");
 			habitacion1.setPrecio((float) 22.11);
-			habitacion1.setObservacion("ojos");
+			habitacion1.setObservacion("ojaaaassosos");
+			habitacion1.setTipo(tipo1);
 			habitacionRepository.save(habitacion1);
 			
-			
+			Tipo tipo2=new Tipo();
+		    tipo1.setId("j002");
+			tipo1.setNombre("jota");
+			tipo1.addHabitacion(habitacion1);
+			tipoRepository.save(tipo2);
+		    
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
